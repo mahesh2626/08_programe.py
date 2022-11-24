d1c902fcbd5daa78f71f04969d0990c72f75dae4
mahesh2626 <118418933+mahesh2626@users.noreply.github.com>
2022-11-24 12:49:24 +0530

    Add files via upload

---
+++ programe.py
@@ -0,0 +1,13 @@
+a=5
+b=7
+print(a+b)
+print(type('a+b'))
+print('_'.isidentifier())
+
+x=y=z=4
+print(x+z)
+print(type(x))
+print(type('z'))
+'''hello world'''
+print('hello python' 
+           'and hello world')
